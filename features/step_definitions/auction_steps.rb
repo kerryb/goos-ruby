f47b159fcf010f8706c234e71da7cc751ab4d493
Kerry Buckley <kerryjbuckley@gmail.com>
2013-06-08 11:26:53 +0100

    Add pending step definitions

---
+++ features/step_definitions/auction_steps.rb
@@ -0,0 +1,7 @@
+Given "an auction of an item is in progress" do
+  pending
+end
+
+When "the auction closes" do
+  pending
+end
---
+++ features/step_definitions/sniper_steps.rb
@@ -0,0 +1,7 @@
+When "I start bidding in the auction" do
+  pending
+end
+
+Then "I should have lost the auction" do
+  pending
+end
