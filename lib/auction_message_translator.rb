2f609b8aef70c44ea4cfb7d9fa7b60b4ab5e6362
Kerry Buckley <kerryjbuckley@gmail.com>
2013-06-15 13:11:19 +0100

    Skeleton spec and implementation for AuctionMessageTranslator

---
+++ lib/auction_message_translator.rb
@@ -0,0 +1,2 @@
+class AuctionMessageTranslator
+end
---
+++ spec/auction_message_translator_spec.rb
@@ -0,0 +1,5 @@
+require "auction_message_translator"
+
+describe AuctionMessageTranslator do
+  it "notifies that the auction is closed when it receives a close message"
+end
