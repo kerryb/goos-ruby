22b428a514bdc1fecdbca64aacc6c008dbebe859
Kerry Buckley <kerryjbuckley@gmail.com>
2013-06-15 10:07:45 +0100

    Extract private method for sending message in fake auction

--- features/support/fake_auction_server.rb
+++ features/support/fake_auction_server.rb
@@ -15,27 +15,23 @@ class FakeAuctionServer
     connect
   end
 
+  def report_price price, increment, current_high_bidder
+    send_message Main::PRICE_EVENT_FORMAT % [price, increment, current_high_bidder]
+  end
+
+  def close
+    send_message ""
+  end
+
   def wait_for_join_request_from_sniper sniper_id
     Timeout.timeout 5 do
       sleep 0.1 until has_received_message?(Main::JOIN_COMMAND_FORMAT, sniper_id)
     end
   end
 
-  def report_price price, increment, current_high_bidder
-    EM.next_tick do
-      client.write Blather::Stanza::Message.new(sniper_id, Main::PRICE_EVENT_FORMAT % [price, increment, current_high_bidder])
-    end
-  end
-
   def wait_for_sniper_to_bid amount, sniper_id
     Timeout.timeout 5 do
       sleep 0.1 until has_received_message?(Main::BID_COMMAND_FORMAT % amount, sniper_id)
-    end
-  end
-
-  def close
-    EM.next_tick do
-      client.write Blather::Stanza::Message.new(sniper_id, "")
     end
   end
 
@@ -57,6 +53,12 @@ class FakeAuctionServer
 
   def sniper_id
     "sniper@localhost"
+  end
+
+  def send_message message
+    EM.next_tick do
+      client.write Blather::Stanza::Message.new(sniper_id, message)
+    end
   end
 
   def has_received_message? text, sender
