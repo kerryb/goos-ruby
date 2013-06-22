9e1bd7a5f73815edf06f75cf2629635e503c7c67
Kerry Buckley <kerryjbuckley@gmail.com>
2013-06-22 19:55:11 +0100

    Remove the other double role spec, and only stub what we expect

--- spec/auction_message_translator_spec.rb
+++ spec/auction_message_translator_spec.rb
@@ -4,16 +4,10 @@ require "auction_message_translator"
 describe AuctionMessageTranslator do
   subject { AuctionMessageTranslator.for sniper_id, auction_event_listener }
   let(:sniper_id) { "sniper@localhost" }
-  let(:auction_event_listener) {
-    double :auction_event_listener, current_price: true, auction_closed: true
-  }
-
-  describe "the auction event listener double used in this spec" do
-    subject { auction_event_listener }
-    it_behaves_like "an auction event listener"
-  end
+  let(:auction_event_listener) { double :auction_event_listener }
 
   it "notifies bid details when it receives a current price message from another bidder" do
+    auction_event_listener.stub :current_price
     message = double :message,
       body: "SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else;"
     subject.call message
@@ -21,6 +15,7 @@ describe AuctionMessageTranslator do
   end
 
   it "notifies bid details when it receives a current price message from the sniper" do
+    auction_event_listener.stub :current_price
     message = double :message,
       body: "SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: #{sniper_id};"
     subject.call message
@@ -28,6 +23,7 @@ describe AuctionMessageTranslator do
   end
 
   it "notifies that the auction is closed when it receives a close message" do
+    auction_event_listener.stub :auction_closed
     message = double :message, body: "SOLVersion: 1.1; Event: CLOSE;"
     subject.call message
     expect(auction_event_listener).to have_received :auction_closed
