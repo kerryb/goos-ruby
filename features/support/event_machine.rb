2b3cbccf8d03fb6c36fd3e7d8493f0491d8a625a
Kerry Buckley <kerryjbuckley@gmail.com>
2013-06-25 18:35:19 +0100

    Wait for EM to be ready after starting

--- features/support/event_machine.rb
+++ features/support/event_machine.rb
@@ -1,8 +1,9 @@
 Before do
   @em_thread = Thread.new { EM.run }
+  sleep 0.01 until EM.reactor_running?
 end
 
 After do
-  EM.stop_event_loop
+  EM.stop_event_loop if EM.reactor_running?
   @em_thread.join
 end
