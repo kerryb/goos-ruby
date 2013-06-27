9f385bea1590af6a458716d2ffc198762cf17b2d
Kerry Buckley <kerryjbuckley@gmail.com>
2013-06-27 21:11:52 +0100

    Remove unused attr_reader

--- lib/ui/main_window.rb
+++ lib/ui/main_window.rb
@@ -1,7 +1,5 @@
 module Ui
   class MainWindow < Gtk::Window
-    attr_reader :status_label
-
     def initialize
       super
 
@@ -11,14 +9,14 @@ module Ui
       end
 
       @status_label = Gtk::Label.new "Joining"
-      add status_label
+      add @status_label
 
       init_ui
       show_all
     end
 
     def show_status status
-      status_label.text = status
+      @status_label.text = status
     end
 
     private
