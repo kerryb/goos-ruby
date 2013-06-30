dcdfa32b1c50a437688186f8b6e4d32fd6759b17
Kerry Buckley <kerryjbuckley@gmail.com>
2013-06-30 09:12:53 +0100

    Rename sniper_status column to sniper_state

--- lib/ui/snipers_table_model.rb
+++ lib/ui/snipers_table_model.rb
@@ -2,7 +2,7 @@ require "gtk2"
 require "sniper_snapshot"
 
 class SnipersTableModel < Gtk::ListStore
-  COLUMNS = [:item_id, :last_price, :last_bid, :sniper_status]
+  COLUMNS = [:item_id, :last_price, :last_bid, :sniper_state]
   def COLUMNS.index_of column
     find_index column
   end
@@ -17,10 +17,10 @@ class SnipersTableModel < Gtk::ListStore
     @row[COLUMNS.index_of :item_id] = sniper_snapshot.item_id
     @row[COLUMNS.index_of :last_price] = sniper_snapshot.last_price
     @row[COLUMNS.index_of :last_bid] = sniper_snapshot.last_bid
-    @row[COLUMNS.index_of :sniper_status] = status_text
+    @row[COLUMNS.index_of :sniper_state] = status_text
   end
 
   def status_text= text
-    @row[COLUMNS.index_of :sniper_status] = text
+    @row[COLUMNS.index_of :sniper_state] = text
   end
 end
--- spec/ui/snipers_table_model_spec.rb
+++ spec/ui/snipers_table_model_spec.rb
@@ -14,7 +14,7 @@ describe SnipersTableModel do
       expect(value_of_column :item_id).to be_empty
       expect(value_of_column :last_price).to be_zero
       expect(value_of_column :last_bid).to eq be_zero
-      expect(value_of_column :sniper_status).to eq "Joining"
+      expect(value_of_column :sniper_state).to eq "Joining"
     end
   end
 
@@ -25,7 +25,7 @@ describe SnipersTableModel do
       expect(value_of_column :item_id).to eq "item-123"
       expect(value_of_column :last_price).to eq 100
       expect(value_of_column :last_bid).to eq 123
-      expect(value_of_column :sniper_status).to eq "Bidding"
+      expect(value_of_column :sniper_state).to eq "Bidding"
     end
   end
 end
