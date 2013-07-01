51db77ebec31d652ff86686e908b7983c2153b6a
Kerry Buckley <kerryjbuckley@gmail.com>
2013-07-01 19:46:41 +0100

    Refactor SniperState spec

--- spec/sniper_state_spec.rb
+++ spec/sniper_state_spec.rb
@@ -1,58 +1,38 @@
 require "sniper_state"
 
-describe SniperState do
-  describe "::JOINING" do
-    subject { SniperState::JOINING }
-    specify "translates as 'Joining'" do
-      expect(subject.to_s).to eq "Joining"
-    end
-
-    specify "transitions to LOST when auction closes" do
-      expect(subject.when_auction_closed).to be SniperState::LOST
-    end
-  end
-
-  describe "::BIDDING" do
-    subject { SniperState::BIDDING }
-    specify "translates as 'Bidding'" do
-      expect(subject.to_s).to eq "Bidding"
-    end
-
-    specify "transitions to LOST when auction closes" do
-      expect(subject.when_auction_closed).to be SniperState::LOST
+{
+  SniperState::JOINING => "Joining",
+  SniperState::BIDDING => "Bidding",
+  SniperState::WINNING => "Winning",
+  SniperState::LOST => "Lost",
+  SniperState::WON => "Won",
+}.each do |state, label|
+  describe state.name do
+    specify "translates as '#{label}'" do
+      expect(state.to_s).to eq label
     end
   end
+end
 
-  describe "::WINNING" do
-    subject { SniperState::WINNING }
-    specify "translates as 'Winning'" do
-      expect(subject.to_s).to eq "Winning"
-    end
-
-    specify "transitions to WON when auction closes" do
-      expect(subject.when_auction_closed).to be SniperState::WON
+{
+  SniperState::JOINING => SniperState::LOST,
+  SniperState::BIDDING => SniperState::LOST,
+  SniperState::WINNING => SniperState::WON,
+}.each do |state, state_after_close|
+  describe state.name do
+    specify "transitions to #{state_after_close.inspect} when auction closes" do
+      expect(state.when_auction_closed).to be state_after_close
     end
   end
+end
 
-  describe "::LOST" do
-    subject { SniperState::LOST }
-    specify "translates as 'Lost'" do
-      expect(subject.to_s).to eq "Lost"
-    end
-
+[
+  SniperState::LOST,
+  SniperState::WON,
+].each do |state|
+  describe state.name do
     specify "does not have a next state" do
-      expect { subject.when_auction_closed }.to raise_error Defect
-    end
-  end
-
-  describe "::WON" do
-    subject { SniperState::WON }
-    specify "translates as 'Won'" do
-      expect(subject.to_s).to eq "Won"
-    end
-
-    specify "does not have a next state" do
-      expect { subject.when_auction_closed }.to raise_error Defect
+      expect { state.when_auction_closed }.to raise_error Defect
     end
   end
 end
