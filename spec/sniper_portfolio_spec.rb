ffac4e0020b62d973e1237950e27649af3cc4b1f
Kerry Buckley <kerryjbuckley@gmail.com>
2013-08-11 10:29:56 +0100

    Add spec for portfolio notifying its listeners

--- spec/sniper_portfolio_spec.rb
+++ spec/sniper_portfolio_spec.rb
@@ -3,4 +3,15 @@ require "support/roles/sniper_collector"
 
 describe SniperPortfolio do
   it_behaves_like "a sniper collector"
+
+  describe "when a sniper is added" do
+    let(:sniper) { double :sniper }
+    let(:listener) { double :listener, sniper_added: true }
+    before { subject.add_portfolio_listener listener }
+
+    it "notifies its listeners" do
+      subject.add_sniper sniper
+      expect(listener).to have_received(:sniper_added).with sniper
+    end
+  end
 end
